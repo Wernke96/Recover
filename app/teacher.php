0d8861a6af1581cc3c560dd4185f805f459951ca
jerred <jerred@lamproslabs.com>
2019-03-02 21:34:15 -0500

    this will add teacher

---
+++ app/teacher.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class teacher extends Model
+{
+    private $teacheruser;
+    private $teacherpasswd;
+
+    public function construct(){}
+    //
+}
