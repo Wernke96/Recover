f4253da23493a590d832f0489dd90e640b353996
jerred <jerred@lamproslabs.com>
2019-03-03 04:42:34 -0500

    this plus

--- app/teacher.php
+++ app/teacher.php
@@ -1,13 +1,18 @@
 <?php
+
 namespace App;
+
 use Illuminate\Database\Eloquent\Model;
 class teacher extends Model
+
 {
     protected $table = "teachers";
     protected $id = "id";
     protected $username="user";
     protected $password="password";
-    
-    
+    protected $email = "email";
+    protected $first ="firstname";
+    protected $last ="lastname";
+
+
     }
-    
