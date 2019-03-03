4d4de3af8201e8d9c0eda540584ac910c037ad1f
Griffin Burkhardt <griffinburkhardt@gmail.com>
2019-03-02 22:36:10 -0500

    oh fuck shit bitch Mobama

---
+++ resources/views/register.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html lang="en">
+   <head>
+      <link rel="stylesheet" href="style.blade.css" type="text/css">
+      <title>ELLo Mate | Student</title>
+      <meta charset="UTF-8">
+      <style>
+         * {
+            box-sizing: border-box
+         }
+
+         header {
+            grid-column: span 3;
+         }
+         body {
+            grid-template-columns: 1fr 1fr 1fr;
+         }
+         .card-container {
+            grid-column: 1;
+            grid-column-start: 2;
+            flex-direction: column;
+            padding: 1em 2em;
+         }
+         #register-header {
+            font-weight: bold;
+            width: 100%;
+            text-align: center;
+         }
+
+         /* Full-width input fields */
+         input[type=text], input[type=password] {
+         width: 100%;
+         padding: 15px;
+         margin-bottom: 2em;
+         display: inline-block;
+         border: none;
+         background: #f1f1f1;
+         }
+
+         input[type=text]:focus, input[type=password]:focus {
+         background-color: #ddd;
+         outline: none;
+         }
+
+         label {
+            font-size: small;
+         }
+
+         /* Set a style for all buttons */
+         button {
+         background-color: #A6A619;
+         color: white;
+         padding: 14px 20px;
+         margin: 8px 0;
+         border: none;
+         border-radius: 5px;
+         cursor: pointer;
+         width: 100%;
+         margin-bottom: 1em;
+         transition: 0.2s all ease-in-out;
+         }
+
+         button:hover {
+         opacity:0.8;
+         }
+      </style>
+   </head>
+   <header>
+      <nav>
+         <h1><span style="color: teal;"><strong>ELL</strong></span><span style="">o</span> Mate</h1>
+      </nav>
+   </header>
+     <div class="card-container">
+      <p id="register-header">Register</p>
+        <form action="action_page.php">
+            <div id="register-container">        
+              <label for="fn">First Name</label>
+              <input type="text" name="fn" required>
+          
+              <label for="ln">Last Name</label>
+              <input type="text" name="ln" required>
+          
+              <label for="username">Username</label>
+              <input type="text" name="username" required>
+
+              <label for="password">Password</label>
+              <input type="text" name="password" required>
+
+               <button type="submit" class="signupbtn">Sign Up</button>
+            </div>
+          </form>
+      </div>
+   <script> </script>
+</html>
---
+++ resources/views/student.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html lang="en">
+   <head>
+      <link rel="stylesheet" href="style.blade.css" type="text/css">
+      <title>ELLo Mate | Student</title>
+      <meta charset="UTF-8">
+      <style>
+         .student-container, .student-fn-ln-container, .stats-container, .stats-level-container {
+            display: flex;
+         }
+         .student-container, .stats-container {
+            width: 50%;
+            padding: 1em 2em;
+            flex-direction: column;
+         }
+         .student-fn, .student-ln {
+            font-weight: bold;
+         }
+         .student-fn-ln-container, .stats-level-container {
+            flex-direction: row;
+         }
+         .student-fn-ln-container p, .stats-level-container p {
+            margin-bottom: 0;
+         }
+      </style>
+
+   </head>
+   <header>
+      <nav>
+         <h1><span style="color: teal;"><strong>ELL</strong></span><span style="">o</span> Mate</h1>
+      </nav>
+   </header>
+     <div class="card-container">
+      <div class="student-container">
+         <div class="student-fn-ln-container">       
+            <p class="student-fn">Griffin</p>
+            <p class="student-ln">Burkhardt</p>
+         </div>
+        <p class="student-username">burkhardtg1</p>
+      </div>
+      <div class="stats-container">
+         <div class="stats-level-container">       
+            <p class="stats-level">Level</p>
+            <p class="level-number">1:</p>
+         </div>
+         <div class="stats-level-container">       
+            <p class="stats-level">Level</p>
+            <p class="level-number">2:</p>
+         </div>
+         <div class="stats-level-container">       
+            <p class="stats-level">Level</p>
+            <p class="level-number">3:</p>
+         </div>
+         <div class="stats-level-container">       
+            <p class="stats-level">Level</p>
+            <p class="level-number">4:</p>
+         </div>
+      </div>
+    </div>
+    <div class="card-container">
+      <div class="student-container">
+        <p class="student-fn">Griffin</p>
+        <p class="student-ln">Burkhardt</p>
+        <p class="student-username">burkhardtg1</p>
+      </div>
+    </div>
+    <div class="card-container">
+      <div class="student-container">
+        <p class="student-fn">Griffin</p>
+        <p class="student-ln">Burkhardt</p>
+        <p class="student-username">burkhardtg1</p>
+      </div>
+    </div>
+   <script> </script>
+</html>
